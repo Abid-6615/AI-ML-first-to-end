4a055f14a162850350b891e65d55002a57b8701f
Abid Sultan Nishan <ariyanabid9999@gmail.com>
2026-04-14 17:51:50 +0600

    Add files via upload

---
+++ text_input.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title(":world_map: First Streamlit App", anchor = False)
+st.header("This is a header", divider = True)
+st.subheader("This is a subheader")
+st.text("This is a text")
+st.markdown("*This is a* :red[**markdown**]")
